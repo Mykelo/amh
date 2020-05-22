148859c9ffab0fcc82e81556ac8fd28043f174b0
Mykelo <michal.mieszko.wawrzyniak@gmail.com>
2020-05-22 17:18:56 +0200

    drobne poprawki

--- l2/z3/main.py
+++ l2/z3/main.py
@@ -6,46 +6,6 @@ from copy import deepcopy
 from map import *
 from path import *
 
-class TabuSearch:
-    def __init__(self, tabulen, path):
-        self.tabulen = tabulen
-        self.tabu = []
-        self.path = path
-
-    def search(self, t, n):
-        random.seed()
-        timeout = time.time() + t
-
-        # Generate path, calculate its cost and remember as the best path
-        currPath = self.path.initPath()
-        bestcost, best = self.path.cost(currPath)
-        while timeout > time.time():
-            # Tweak the current path and calculate cost
-            currPath = self.path.tweak(currPath)
-            newcost, currPath = self.path.cost(currPath)
-
-            # Generate n neighbours of the current path and try to find a better solution
-            for _ in range(n):
-                if timeout <= time.time():
-                    break
-                
-                neighbour = self.path.tweak(currPath)
-                neigbourcost, neighbour = path.cost(neighbour)
-
-                if neighbour not in self.tabu and neigbourcost < newcost:
-                    newcost = neigbourcost
-                    currPath = neighbour
-                    self.tabu.append(currPath)
-
-                # Control the length of the tabu list
-                if len(self.tabu) > self.tabulen:
-                    self.tabu = self.tabu[1:]
-            if newcost < bestcost:
-                bestcost = newcost
-                best = currPath
-
-        return best
-
 def simAnnealing(maxTime, t, c, path):
     timeout = time.time() + maxTime
     s = path.initPath()
